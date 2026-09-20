74386391bab10f9b66947daf22011cf0d28c92ec
ridhimam-3 <Ridhima.38720@stu.upes.ac.in>
2026-09-20 19:37:51 +0530

    Update and rename q2_100.c to q02_100.c

--- Assignment/q02_100.c
+++ Assignment/q02_100.c
@@ -1,4 +1,18 @@
-#include <stdio.h>
+//Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+/*
+Sample Test Cases:
+Input 1:
+10 2
+Output 1:
+Sum=12, Diff=8, Product=20, Quotient=5
+
+Input 2:
+7 3
+Output 2:
+Sum=10, Diff=4, Product=21, Quotient=2
+
+*/#include <stdio.h>
 int main() 
 {
     float num1, num2;
